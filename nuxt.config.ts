8e95ac63ffb30a05d084bca9c1d1079bb21b7ad3
nguyendangduy <nguyendangduy269@gmail.com>
2023-08-03 16:25:35 +0700

    add some code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
+const path = require('path');
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false
+  ssr: false,
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'dist')
+    }
+  }
 })
