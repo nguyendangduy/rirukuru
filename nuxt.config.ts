b27fabfa8e19ebe41ef08a1145b3cbf5b53c9d6f
nguyendangduy <nguyendangduy269@gmail.com>
2023-08-07 09:27:28 +0700

    chore: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
-const path = require('path')
+import path from 'path'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
-  css: [
-    '@/assets/scss/style.scss',
-  ],
+  css: ['@/assets/scss/style.scss'],
+  modules: ['@nuxtjs/eslint-config-typescript'],
   nitro: {
     preset: 'service-worker',
     output: {
@@ -14,4 +13,3 @@ export default defineNuxtConfig({
     },
   },
 })
- 
