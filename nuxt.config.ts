1b0c51a3a77b13d358298023fa91654a4308ec53
nguyendangduy <nguyendangduy269@gmail.com>
2023-08-29 11:46:29 +0700

    fix: build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   ssr: false,
   css: ['@/assets/scss/style.scss'],
   nitro: {
